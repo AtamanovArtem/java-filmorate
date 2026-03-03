6817043696c40180e27b5b90722f685876e1ae0f
Artem Atamanov <aassvpz@mail.ru>
2026-03-03 22:40:15 +0300

    добавлены модификаторы доступа классов User и Film

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -9,9 +9,9 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @Builder
 public class Film {
-	int id;
-	String name;
-	String description;
-	LocalDate releaseDate;
-	int duration;
+	private int id;
+	private String name;
+	private String description;
+	private LocalDate releaseDate;
+	private int duration;
 }
