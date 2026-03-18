995c985e340719471b99e89631ea6ea4585ce646
Artem Atamanov <aassvpz@mail.ru>
2026-03-18 18:52:01 +0300

    убраны неиспользуемые импорты во всех классах

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -1,15 +1,19 @@
 package ru.yandex.practicum.filmorate.storage.film;
 
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.User;
 
 import java.util.Collection;
 
 public interface FilmStorage {
 	void addFilm(Film film);
+
 	void removeFilm(Integer filmId);
+
 	void updateFilm(Film film);
+
 	Collection<Film> getAllFilms();
+
 	boolean contains(long filmId);
+
 	Film getFilmById(long id);
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -6,9 +6,14 @@ import java.util.Collection;
 
 public interface UserStorage {
 	void addUser(User user);
+
 	void removeUser(Long userId);
+
 	void updateUser(User user);
+
 	Collection<User> getAllUsers();
+
 	boolean contains(long userId);
+
 	User getUserById(long id);
 }
