2a3e3b328e6e551013eae3279f4baeab6552dc8b
Artem Atamanov <aassvpz@mail.ru>
2026-03-18 18:48:18 +0300

    убраны неиспользуемые импорты в UserController

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -5,7 +5,6 @@ import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.UserService;
 
 import java.util.Collection;
-import java.util.Set;
 
 
 @RestController
